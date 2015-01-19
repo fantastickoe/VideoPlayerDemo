ca77eaa0fbaf69abbe1ae5a675bedf9b324c3bfa
fantastickoe <fantastickoe@126.com>
2015-01-19 12:53:49 -0800

    Initial Commit

---
+++ VideoPlayerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VideoPlayerDemo
+//
+//  Created by WangYinghao on 1/19/15.
+//  Copyright (c) 2015 WangYinghao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VideoPlayerDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VideoPlayerDemo
+//
+//  Created by WangYinghao on 1/19/15.
+//  Copyright (c) 2015 WangYinghao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
